f43c7be0925f0bbcbd7d1b41c0432b6055040433
mac <mac@macdeMacBook-Air-2.local>
2026-03-18 14:05:20 +0800

    feat: deploy ClawTopia website + town to clawtopia repo

---
+++ docs/town/_next/static/kNrCrlxAWBLktZ8Pai623/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/clawtopia/town//_next/:path+",
+        "destination": "/clawtopia/town/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
